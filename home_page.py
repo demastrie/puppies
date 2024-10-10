70b3ae8cac90d56ca6f01a4c03e4fa671e27e2fe
Michael Demastrie <michael@dematrie.com>
2024-10-10 10:37:27 -0400

    Reformatted python for PEP8. Updated weights.

--- home_page.py
+++ home_page.py
@@ -1,56 +1,61 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-PAGE_TITLE = 'Puppy Weight Tracker'
+PAGE_TITLE = "Puppy Weight Tracker"
 st.set_page_config(
-        page_title=PAGE_TITLE,
+    page_title=PAGE_TITLE,
 )
 st.title(PAGE_TITLE)
-df_puppy_weights = pd.read_csv('puppy-weights.csv')
+df_puppy_weights = pd.read_csv("puppy-weights.csv")
 df_puppy_weights = df_puppy_weights.dropna()
-df_puppy_weights['date'] = pd.to_datetime(df_puppy_weights['date'])
-df_puppy_weights['weight-oz'] = df_puppy_weights['weight-gm'] * 0.03527396
+df_puppy_weights["date"] = pd.to_datetime(df_puppy_weights["date"])
+df_puppy_weights["weight-oz"] = df_puppy_weights["weight-gm"] * 0.03527396
+
 
 # Add a pounds and ounces column
-
-
 def format_weight(row):
-    '''format_weight(row) returns the weight in pounds and ounces'''
+    """format_weight(row) returns the weight in pounds and ounces"""
 
-    pounds_str = f"{row['pounds']} lb " if row['pounds'] > 0 else ""
-    ounces_str = f"{row['ounces']} oz" if row['ounces'] > 0 else ""
+    pounds_str = f"{row['pounds']} lb " if row["pounds"] > 0 else ""
+    ounces_str = f"{row['ounces']} oz" if row["ounces"] > 0 else ""
     return pounds_str + ounces_str
 
 
-df_puppy_weights['pounds_float'] = df_puppy_weights['weight-oz'] / 16
-df_puppy_weights['pounds'] = df_puppy_weights['pounds_float'].astype(int)
-df_puppy_weights['ounces'] = ((df_puppy_weights['pounds_float']
-                               - df_puppy_weights['pounds']) * 16) \
-                                .round(2).astype(float)
-df_puppy_weights['weight-lboz'] = df_puppy_weights.apply(format_weight, axis=1)
-df_puppy_weights.drop(['pounds_float', 'pounds', 'ounces'],
-                      axis=1, inplace=True)
+df_puppy_weights["pounds_float"] = df_puppy_weights["weight-oz"] / 16
+df_puppy_weights["pounds"] = df_puppy_weights["pounds_float"].astype(int)
+df_puppy_weights["ounces"] = (
+    ((df_puppy_weights["pounds_float"] - df_puppy_weights["pounds"]) * 16)
+    .round(2)
+    .astype(float)
+)
+df_puppy_weights["weight-lboz"] = df_puppy_weights.apply(format_weight, axis=1)
+df_puppy_weights.drop(["pounds_float", "pounds", "ounces"], axis=1, inplace=True)
 
 # shape data and show a line chart
-df_puppy_weights_pivoted = df_puppy_weights.pivot(index='date',
-                                                  columns='name')['weight-gm']
+df_puppy_weights_pivoted = df_puppy_weights.pivot(index="date", columns="name")[
+    "weight-gm"
+]
 
-earliest_date = df_puppy_weights['date'].min()
-df_puppy_weights['days-old'] = df_puppy_weights['date'] - earliest_date
-df_puppy_weights['days-old'] = df_puppy_weights['days-old'].dt.days.astype(int)
+earliest_date = df_puppy_weights["date"].min()
+df_puppy_weights["days-old"] = df_puppy_weights["date"] - earliest_date
+df_puppy_weights["days-old"] = df_puppy_weights["days-old"].dt.days.astype(int)
 
 df_puppy_weights_pivoted.columns.name = None
 
-st.line_chart(df_puppy_weights_pivoted, y=df_puppy_weights_pivoted.columns,
-              x_label="2024", y_label="Weight in grams")
-st.dataframe(df_puppy_weights_pivoted
-             .sort_values(by=['date'], ascending=False))
+st.line_chart(
+    df_puppy_weights_pivoted,
+    y=df_puppy_weights_pivoted.columns,
+    x_label="2024",
+    y_label="Weight in grams",
+)
+st.dataframe(df_puppy_weights_pivoted.sort_values(by=["date"], ascending=False))
 
 # show the larger dataframe
-if st.checkbox('Show all puppy weight data', value=True):
+if st.checkbox("Show all puppy weight data", value=True):
     names = df_puppy_weights.name.unique()
-    puppies = st.multiselect("Names: ",
-                             names,
-                             names)
-    st.dataframe(df_puppy_weights.loc[df_puppy_weights.name.isin(puppies)]
-                 .sort_values(by=['date'], ascending=False))
+    puppies = st.multiselect("Names: ", names, names)
+    st.dataframe(
+        df_puppy_weights.loc[df_puppy_weights.name.isin(puppies)].sort_values(
+            by=["date"], ascending=False
+        )
+    )
